648e3338788e2a1369816ab8e6e54f17a8986e97
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-04-07 19:49:56 -0700

    fixing css and html

--- static/js/modules/components/ModifyDiv/AbsoluteCardTopHalf.js
+++ static/js/modules/components/ModifyDiv/AbsoluteCardTopHalf.js
@@ -2,7 +2,7 @@ import MakeItNormalIcon from "../Icons/MakeItNormalIcon.js";
 
 
 const AbsoluteCardTopHalf = `<div class="cardTopHalf">
-                                <span class="button editButton"><i class="fa-solid fa-pen-to-square"></i></span>
+<!--                                <span class="button editButton"><i class="fa-solid fa-pen-to-square"></i></span>-->
                                 <span class="button makeItNormalButton">${MakeItNormalIcon}</span>
                                 <span class="button deleteButton"><i class="fa-solid fa-trash-can"></i></span>
                             </div>`
--- static/js/modules/components/ModifyDiv/NormalCardTopHalf.js
+++ static/js/modules/components/ModifyDiv/NormalCardTopHalf.js
@@ -2,7 +2,7 @@ import MakeItAbsoluteIcon from "../Icons/MakeItAbsoluteIcon.js";
 
 
 const NormalCardTopHalf = `<div class="cardTopHalf">
-                                <span class="button editButton"><i class="fa-solid fa-pen-to-square"></i></span>
+<!--                                <span class="button editButton"><i class="fa-solid fa-pen-to-square"></i></span>-->
                                 <span class="button makeItAbsoluteButton">${MakeItAbsoluteIcon}</span>
                                 <span class="button deleteButton"><i class="fa-solid fa-trash-can"></i></span>
                             </div>`
